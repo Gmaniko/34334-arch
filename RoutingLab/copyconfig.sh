21f5f8b2eac06cfbe68ad4d62262f5a0ba71ab43
Gmaniko <niko220302@gmail.com>
2022-10-24 18:20:10 +0200

    ospf added

--- RoutingLab/copyconfig.sh
+++ RoutingLab/copyconfig.sh
@@ -5,12 +5,23 @@ if [ -n "$(sudo docker ps -f "name=r14" -q)" ]
 then
 	sudo docker cp router1/ripd.conf router1:/etc/quagga/ripd.conf
 	sudo docker cp router1/zebra.conf router1:/etc/quagga/zebra.conf
+	sudo docker cp router1/ospfd.conf router1:/etc/quagga/ospfd.conf
+	sudo docker cp router1/daemons router1:/etc/quagga/daemons
+
 	sudo docker cp router2/ripd.conf router2:/etc/quagga/ripd.conf
 	sudo docker cp router2/zebra.conf router2:/etc/quagga/zebra.conf
+	sudo docker cp router2/ospfd.conf router2:/etc/quagga/ospfd.conf
+	sudo docker cp router2/daemons router2:/etc/quagga/daemons
+
 	sudo docker cp router3/ripd.conf router3:/etc/quagga/ripd.conf
 	sudo docker cp router3/zebra.conf router3:/etc/quagga/zebra.conf
+	sudo docker cp router3/ospfd.conf router3:/etc/quagga/ospfd.conf
+	sudo docker cp router3/daemons router3:/etc/quagga/daemons
+
 	sudo docker cp router4/ripd.conf router4:/etc/quagga/ripd.conf
 	sudo docker cp router4/zebra.conf router4:/etc/quagga/zebra.conf
+	sudo docker cp router4/ospfd.conf router4:/etc/quagga/ospfd.conf
+	sudo docker cp router4/daemons router4:/etc/quagga/daemons
 
 	sudo docker exec -ti router1 service quagga restart
 	sudo docker exec -ti router2 service quagga restart
