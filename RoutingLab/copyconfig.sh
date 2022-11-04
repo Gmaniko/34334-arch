bcc46a89d3225d246a424593b66bf3efa2be1d32
Gmaniko <niko220302@gmail.com>
2022-11-04 15:27:20 +0100

    bgp

--- RoutingLab/copyconfig.sh
+++ RoutingLab/copyconfig.sh
@@ -6,6 +6,7 @@ then
 	sudo docker cp router1/ripd.conf router1:/etc/quagga/ripd.conf
 	sudo docker cp router1/zebra.conf router1:/etc/quagga/zebra.conf
 	sudo docker cp router1/ospfd.conf router1:/etc/quagga/ospfd.conf
+	sudo docker cp router1/bgpd.conf router1:/etc/quagga/bgp.conf
 	sudo docker cp router1/daemons router1:/etc/quagga/daemons
 
 	sudo docker cp router2/ripd.conf router2:/etc/quagga/ripd.conf
@@ -16,6 +17,7 @@ then
 	sudo docker cp router3/ripd.conf router3:/etc/quagga/ripd.conf
 	sudo docker cp router3/zebra.conf router3:/etc/quagga/zebra.conf
 	sudo docker cp router3/ospfd.conf router3:/etc/quagga/ospfd.conf
+	sudo docker cp router3/bgpd.conf router3:/etc/quagga/bgpd.conf
 	sudo docker cp router3/daemons router3:/etc/quagga/daemons
 
 	sudo docker cp router4/ripd.conf router4:/etc/quagga/ripd.conf
